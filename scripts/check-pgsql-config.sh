cf443ccbfd0d52282b9e4dfa5be1b7d4015ed32c
Jack Moon <rokmc756@gmail.com>
2025-05-01 00:54:52 +0900

    Removed Files

--- scripts/check-pgsql-config.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-for i in `echo 81 82 83`
-do
-    ssh root@192.168.0.$i "cat /var/lib/pgsql/9.4-bdr/data/postgresql.conf"
-    ssh root@192.168.0.$i "cat /var/lib/pgsql/9.4-bdr/data/pg_hba.conf"
-done
-
